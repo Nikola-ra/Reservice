e529fdebc71a58801c6d4b93f9d51247de64df0b
cp <criprecripre97@gmail.com>
2023-04-04 21:15:22 +0200

    "Update tattico di css"

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,16 @@ const addScroll = () => {
     }
 }
 
+angolo = 0;
+const animateBackground = ()=>{
+    angolo = angolo+1;
+    if (angolo == 360){
+        angolo = 0;
+    }
+    document.querySelector("#mainSegment").style.background = 'linear-gradient('+ angolo + 'deg, rgba(98,142,224,1) 0%, rgba(92,204,237,1) 100%)';
+}
+setInterval(animateBackground,100)
+
 document.querySelector('#burger').addEventListener('click',()=>{
     document.body.classList.toggle('menu-open')
 })
