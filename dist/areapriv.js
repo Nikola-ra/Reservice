bc2186b1a2ca3d2482492b27601f13398debf425
Ssaverio <gio.brignoli2006@gmail.com>
2023-04-16 00:11:39 +0200

    Routing area privata
    Aggiunta base di routing delle varie pagine accessibili ad ogni azienda

--- dist/areapriv.js
+++ dist/areapriv.js
@@ -1,3 +1,34 @@
-const areaSections = document.querySelectorAll('.area-section')
+class Route {
+    constructor(pagina,icona) {
+        this.pagina = pagina
+        this.icona = icona
+    }
 
-console.log(areaSections)
+    routeTo() {this.pagina.style.display = 'block'}
+}
+
+const dashboard = new Route(document.querySelector('#dashboard-p'),document.querySelector('#dashboard-i'))
+const agenda = new Route(document.querySelector('#agenda-p'),document.querySelector('#agenda-i'))
+const analitica = new Route(document.querySelector('#analitica-p'),document.querySelector('#analitica-i'))
+const abbonamenti = new Route(document.querySelector('#abbonamenti-p'),document.querySelector('#abbonamenti-i'))
+const recensioni = new Route(document.querySelector('#recensioni-p'),document.querySelector('#recensioni-i'))
+
+const router = [dashboard,agenda,analitica,abbonamenti,recensioni]
+
+const clearAllRoutes = (sections) => {
+    sections.forEach((section)=>{
+        section.pagina.style.display = 'none'
+        section.icona.classList.remove('border-hover')
+        section.icona.classList.add('border-not-hover')
+    })
+}
+
+router.forEach((route) => {
+    route.icona.addEventListener('click',()=>{
+        clearAllRoutes(router)
+        route.routeTo()
+        document.body.classList.remove('menu-open')
+        route.icona.classList.remove('border-not-hover')
+        route.icona.classList.add('border-hover')
+    })
+})
