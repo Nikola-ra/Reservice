eeb73aa2e9980e46c06a88cb7386465c382065b9
Ssaverio <gio.brignoli2006@gmail.com>
2023-04-26 22:01:07 +0200

    area utenti

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,3 @@
-const addScroll = () => {
-    if (window.scrollY > 30) {
-        document.body.classList.add('scroll')
-    } else {
-        document.body.classList.remove('scroll')
-    }
-}
-
 angolo = 0
 const animateBackground = ()=>{
     angolo += 1
@@ -13,8 +5,3 @@ const animateBackground = ()=>{
     document.querySelector("#mainSegment").style.background = 'linear-gradient('+ angolo + 'deg, rgba(98,142,224,1) 0%, rgba(92,204,237,1) 100%)'
 }
 setInterval(animateBackground,100)
-
-document.querySelector('#burger').addEventListener('click',()=>{document.body.classList.toggle('menu-open')})
-
-document.addEventListener('DOMContentLoaded',addScroll)
-document.addEventListener('scroll',addScroll)
